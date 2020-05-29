6db00e4691234f9880c6e3fd2092816e4e87318f
nireo <eemil.lehtikangas1@gmail.com>
2020-05-29 21:17:45 +0300

    added specific ports flag

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"sort"
+
+	"github.com/nireo/port-scanner/utils"
 )
 
 func worker(ports, results chan int) {
@@ -19,31 +22,66 @@ func worker(ports, results chan int) {
 }
 
 func main() {
-	ports := make(chan int, 100)
-	results := make(chan int)
-	var openPorts []int
+	var portsString string
+	flag.StringVar(&portsString, "ports", "", "enter specific ports to be scanned separated by commans i.e: 1,8008,3001")
 
-	for i := 0; i < cap(ports); i++ {
-		go worker(ports, results)
-	}
+	if portsString == "" {
+		ports := make(chan int, 100)
+		results := make(chan int)
+		var openPorts []int
 
-	go func() {
-		for i := 1; i <= 1024; i++ {
-			ports <- i
+		for i := 0; i < cap(ports); i++ {
+			go worker(ports, results)
 		}
-	}()
 
-	for i := 0; i < 1024; i++ {
-		port := <-results
-		if port != 0 {
-			openPorts = append(openPorts, port)
+		go func() {
+			for i := 1; i <= 1024; i++ {
+				ports <- i
+			}
+		}()
+
+		for i := 0; i < 1024; i++ {
+			port := <-results
+			if port != 0 {
+				openPorts = append(openPorts, port)
+			}
+		}
+
+		close(ports)
+		close(results)
+		sort.Ints(openPorts)
+		for _, port := range openPorts {
+			fmt.Printf("%d open\n", port)
+		}
+	} else {
+		formattedPorts := utils.SeparatePortsFromString(portsString)
+		ports := make(chan int, 100)
+		results := make(chan int)
+		var openPorts []int
+
+		for i := 0; i < cap(ports); i++ {
+			go worker(ports, results)
+		}
+
+		go func() {
+			for _, value := range formattedPorts {
+				ports <- value
+			}
+		}()
+
+		for i := 0; i < len(formattedPorts); i++ {
+			port := <-results
+			if port != 0 {
+				openPorts = append(openPorts, port)
+			}
+		}
+
+		close(ports)
+		close(results)
+		sort.Ints(openPorts)
+		for _, port := range openPorts {
+			fmt.Printf("%d open\n", port)
 		}
 	}
 
-	close(ports)
-	close(results)
-	sort.Ints(openPorts)
-	for _, port := range openPorts {
-		fmt.Printf("%d open\n", port)
-	}
 }
